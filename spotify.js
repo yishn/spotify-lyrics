dee48be36fb5f04ecd98575d3ec4512067aa124e
Yichuan Shen <shen.yichuan@gmail.com>
2016-10-26 18:08:15 +0200

    Use polling

--- spotify.js
+++ spotify.js
@@ -2,20 +2,22 @@ const deepEqual = require('deep-equal')
 const spotify = new require('node-spotify-webhelper').SpotifyWebHelper()
 const EventEmitter = require('events')
 
-let loopId, lastSong
+let loopId, lastSongId
 
 module.exports = exports = new EventEmitter()
+
+let getTrackId = track => ['track', 'artist', 'album'].map(x => track[x + '_resource'].uri)
 
 exports.start = function() {
     let loop = () => spotify.getStatus((err, result) => {
         if (err || result.error || !result.running) return
 
         clearTimeout(loopId)
-        loopId = setTimeout(loop, (result.track.length - result.playing_position) * 1000 + 100)
+        loopId = setTimeout(loop, Math.min(1000, (result.track.length - result.playing_position) * 1000))
 
-        if (deepEqual(lastSong, result.track)) return
+        if (deepEqual(lastSongId, getTrackId(result.track))) return
 
-        lastSong = result.track
+        lastSongId = getTrackId(result.track)
         exports.emit('song-update', result)
     })
 
