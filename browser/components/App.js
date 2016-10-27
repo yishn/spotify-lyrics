987a074ffc6f04348bc5ba75c427f545a5f5b86a
Yichuan Shen <shen.yichuan@gmail.com>
2016-10-27 22:06:02 +0200

    Implement auto scrolling

--- browser/components/App.js
+++ browser/components/App.js
@@ -10,13 +10,15 @@ class App extends Component {
     constructor() {
         super()
 
-        spotify.on('song-update', ({track}) => {
+        spotify.on('song-update', ({track, playing_position}) => {
             this.state = {}
             this.setState({
                 loading: true,
                 title: track.track_resource.name,
                 artists: [track.artist_resource.name],
-                album: track.album_resource.name
+                album: track.album_resource.name,
+                position: playing_position,
+                total: track.length
             })
 
             let query = [this.state.title, this.state.artists].join(' ')
@@ -29,15 +31,22 @@ class App extends Component {
             })
         })
 
+        spotify.on('song-progress', ({track, playing_position}) => {
+            this.setState({
+                position: playing_position,
+                total: track.length
+            })
+        })
+
         window.addEventListener('load', () => spotify.listen())
     }
 
-    render({}, {loading, title, artists, album, art, lyrics, url}) {
+    render({}, {loading, title, artists, album, art, lyrics, url, position, total}) {
         return h('div', {id: 'root'},
             h(TitleBar),
             h(TrackInfo, {loading, title, artists, album, art}),
             h('main', {},
-                h(LyricsBox, {loading, lyrics, url}),
+                h(LyricsBox, {loading, lyrics, url, position, total}),
                 h('div', {class: 'fade-in'}),
                 h('div', {class: 'fade-out'})
             )
--- browser/components/LyricsBox.js
+++ browser/components/LyricsBox.js
@@ -1,37 +1,72 @@
 const {shell} = require('electron')
-const {h} = require('preact')
+const {h, Component} = require('preact')
+const scroll = require('scroll')
 const smartypants = require('../../modules/smartypants')
 
 let words
 
-module.exports = ({loading, lyrics, url}) =>
-
-h('section', {class: {'lyrics-box': true, loading}},
-    h('p', {class: 'placeholder'}),
-    lyrics
-    ? smartypants(lyrics).split('\n').map(line =>
-        (line = line.trim()) == ''
-        ? h('br')
-        : h('p', {class: {parentheses: line[0] == '(' && line[line.length - 1] == ')'}},
-            (words = line.split(/\s+/)).slice(0, words.length - 2).join(' ') + ' ',
-            h('span', {}, words.splice(-2).join(' '))
-        )
-    )
-    : loading
-    ? h('div', {class: 'spinner'}, h('div'), h('div'), h('div'))
-    : h('p', {class: 'no-lyrics'}, 'No Lyrics'),
-
-    lyrics && h('p', {class: 'end'}, 'End'),
-
-    lyrics && h('a', {
-        class: 'badge',
-        href: '#',
-        title: 'Powered by Musixmatch',
-        onclick: e => {
-            e.preventDefault()
-            shell.openExternal(url)
+class LyricsBox extends Component {
+    componentDidUpdate() {
+        if (this.props.loading) {
+            this.lastScrollTop = null
+            this.lastTimePosition = null
+            return
         }
-    }, h('img', {src: 'img/badge.svg'})),
 
-    lyrics && h('p', {class: 'placeholder'})
-)
+        let userChanged = this.lastScrollTop != null && Math.abs(this.element.scrollTop - this.lastScrollTop) > 10
+        let scrollTop = this.element.scrollTop
+
+        if (!userChanged) {
+            let deltaTime = this.props.position - this.lastTimePosition
+            let lastRemaining = this.props.total - this.lastTimePosition
+            let progress = deltaTime / lastRemaining
+            let remainingPx = this.element.scrollHeight - this.element.clientHeight - this.element.scrollTop
+            let deltaPx = progress * remainingPx
+
+            scrollTop += deltaPx
+        }
+
+        this.lastScrollTop = scrollTop
+        this.lastTimePosition = this.props.position
+        scroll.top(this.element, scrollTop)
+    }
+
+    render({loading, lyrics, url, progress}) {
+        return h('section', {
+            class: {'lyrics-box': true, loading},
+            ref: el => this.element = el,
+            'data-progress': progress
+        }, [
+            lyrics && h('p', {class: 'placeholder'}),
+
+            lyrics
+            ? smartypants(lyrics).split('\n').map(line =>
+                (line = line.trim()) == ''
+                ? h('br')
+                : h('p', {class: {parentheses: line[0] == '(' && line[line.length - 1] == ')'}},
+                    (words = line.split(/\s+/)).slice(0, words.length - 2).join(' ') + ' ',
+                    h('span', {}, words.splice(-2).join(' '))
+                )
+            )
+            : loading
+            ? h('div', {class: 'spinner'}, h('div'), h('div'), h('div'))
+            : h('p', {class: 'no-lyrics'}, 'No Lyrics'),
+
+            lyrics && h('p', {class: 'end'}, 'End'),
+
+            lyrics && h('a', {
+                class: 'badge',
+                href: '#',
+                title: 'Powered by Musixmatch',
+                onclick: e => {
+                    e.preventDefault()
+                    shell.openExternal(url)
+                }
+            }, h('img', {src: 'img/badge.svg'})),
+
+            lyrics && h('p', {class: 'placeholder'})
+        ])
+    }
+}
+
+module.exports = LyricsBox
