b2c8f8325269e5330d5b39481772eeffa30826f2
Yichuan Shen <shen.yichuan@gmail.com>
2016-10-27 09:32:25 +0200

    Add TrackInfo component

--- browser/components/App.js
+++ browser/components/App.js
@@ -1,10 +1,17 @@
 const {h, render, Component} = require('preact')
 const TitleBar = require('./TitleBar')
+const TrackInfo = require('./TrackInfo')
 
 class App extends Component {
     render() {
         return h('div', {id: 'root'},
-            h(TitleBar, {title: 'Spotify Lyrics'})
+            h(TitleBar, {title: 'Spotify Lyrics'}),
+            h(TrackInfo, {
+                title: 'Million Reasons',
+                artists: ['Lady Gaga'],
+                album: 'Joanne',
+                art: 'https://s.mxmcdn.net/images-storage/albums/7/4/4/4/7/1/35174447_350_350.jpg'
+            })
         )
     }
 }
---
+++ browser/components/TrackInfo.js
@@ -0,0 +1,16 @@
+const {h, render} = require('preact')
+
+module.exports = ({title, artists, album, art}) => h('section', {class: 'track-info'},
+    h('div', {class: 'drag'}),
+    h('img', {
+        class: 'art',
+        width: 67,
+        height: 67,
+        src: art
+    }),
+    h('ul', {},
+        h('li', {class: 'title'}, title),
+        h('li', {class: 'artists'}, artists.join(', ')),
+        h('li', {class: {album: true, disabled: !album}}, album ? album : 'No Album')
+    )
+)
