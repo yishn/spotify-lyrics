e8aeb04fa9bef58ac245528e14c0355aa316b00d
Yichuan Shen <shen.yichuan@gmail.com>
2016-11-02 16:03:01 +0100

    TrackInfo uses Component class

--- browser/components/TrackInfo.js
+++ browser/components/TrackInfo.js
@@ -1,26 +1,34 @@
-const {h} = require('preact')
+const {h, Component} = require('preact')
 const smartypants = require('../../modules/smartypants')
 
-module.exports = ({loading, title, artists, album, art}) =>
+class TrackInfo extends Component {
+    render({loading, title, artists, album, art}) {
+        let titleText = smartypants(title || (loading ? '…' : 'No Title'))
+        let artistsText = smartypants(artists && artists.length ? artists.join(', ') : (loading ? '…' : 'No Artists'))
+        let albumText = smartypants(album || (loading ? '…' : 'No Album'))
 
-h('section', {class: {'track-info': true, loading}},
-    h('div', {class: 'drag'}),
-    h('img', {
-        class: 'art',
-        src: 'img/blank.svg',
-        style: {
-            backgroundImage: art ? `url('${art}')` : "url('img/blank.svg')"
-        }
-    }),
-    h('ul', {},
-        h('li', {class: {title: true, disabled: !title}},
-            smartypants(title || (loading ? '…' : 'No Title'))
-        ),
-        h('li', {class: {artists: true, disabled: !artists || !artists.length}},
-            smartypants(artists && artists.length ? artists.join(', ') : (loading ? '…' : 'No Artists'))
-        ),
-        h('li', {class: {album: true, disabled: !album}},
-            smartypants(album || (loading ? '…' : 'No Album'))
+        return h('section', {class: {'track-info': true, loading}},
+            h('div', {class: 'drag'}),
+            h('img', {
+                class: 'art',
+                src: 'img/blank.svg',
+                style: {
+                    backgroundImage: art ? `url('${art}')` : "url('img/blank.svg')"
+                }
+            }),
+            h('ul', {},
+                h('li', {class: {title: true, disabled: !title}, title: titleText},
+                    titleText
+                ),
+                h('li', {class: {artists: true, disabled: !artists || !artists.length}, title: artistsText},
+                    artistsText
+                ),
+                h('li', {class: {album: true, disabled: !album}, title: albumText},
+                    albumText
+                )
+            )
         )
-    )
-)
+    }
+}
+
+module.exports = TrackInfo
