d3a1834c8ef9b5c031faef467331f7ab5618249b
Yichuan Shen <shen.yichuan@gmail.com>
2017-03-11 22:28:55 +0100

    Remove deepEqual dependency

--- modules/spotify.js
+++ modules/spotify.js
@@ -1,4 +1,3 @@
-const deepEqual = require('deep-equal')
 const spotify = new require('node-spotify-webhelper').SpotifyWebHelper()
 const EventEmitter = require('events')
 
@@ -7,6 +6,7 @@ let loopId, lastSongId
 module.exports = exports = new EventEmitter()
 
 let getTrackId = track => ['track', 'artist', 'album'].map(x => track[x + '_resource'].uri)
+let equals = (a, b) => a.length === b.length && a.every((x, i) => x === b[i])
 
 exports.listen = function() {
     let loop = () => spotify.getStatus((err, result) => {
@@ -15,7 +15,7 @@ exports.listen = function() {
         clearTimeout(loopId)
         loopId = setTimeout(loop, Math.min(result.track.length - result.playing_position, 1) * 1000)
 
-        if (deepEqual(lastSongId, getTrackId(result.track)))
+        if (equals(lastSongId, getTrackId(result.track)))
             return exports.emit('song-progress', result)
 
         lastSongId = getTrackId(result.track)
