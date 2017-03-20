cdc3c7378806f320d56ace40ba3aad1a61221670
Yichuan Shen <shen.yichuan@gmail.com>
2017-03-20 23:02:47 +0100

    Update smartypants to recognize ` and ´ as apostrophes

--- modules/smartypants.js
+++ modules/smartypants.js
@@ -1,9 +1,9 @@
 module.exports = function(text) {
     return text
         .replace(/\.{3}/g, '…')
-        .replace(/(\S)'/g, '$1’')
+        .replace(/(\S)('|´)/g, '$1’')
         .replace(/(\S)"/g, '$1”')
-        .replace(/'(\S)/g, '‘$1')
+        .replace(/('|`)(\S)/g, '‘$2')
         .replace(/"(\S)/g, '“$1')
         .replace(/(\s)-(\s)/g, '$1–$2')
 }
